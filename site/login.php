7a69eb07f9095d803d5c3f5fbacbe48d92f1f306
ThiagoEF <117860635+ThiagoEF@users.noreply.github.com>
2022-11-29 10:13:17 -0300

    Add files via upload

--- site/login.php
+++ site/login.php
@@ -4,8 +4,8 @@
 <div id="login">
     <form action="loginback.php" method="post">
         <h2 id="h2">LOGIN</h2>
-        <input class="input"type="text" name="username" placeholder="username">
-        <input class="input"type="password" name="password" placeholder="password">
+        <input class="input"type="text" required name="username" placeholder="username">
+        <input class="input"type="password" required name="password" placeholder="password">
         <button id="button-login">Enviar</button>
     </form>
 </div>
--- site/loginback.php
+++ site/loginback.php
@@ -7,8 +7,8 @@ $data = mysqli_query($con,"SELECT * FROM users WHERE id = 1 ");
 $pass = $_POST['password'];
 $user = $_POST['username'];
 
-$cor_pass = mysqli_fetch_assoc($data)['pass'];
-$cor_user = "admi";
+$cor_pass = mysqli_fetch_assoc($data['pass']);
+$cor_user = mysqli_fetch_assoc($data['user']);
 
 if ($pass != $cor_user || $user != $cor_pass){
     echo "<p>Invalid, <a href='index.php?pg=login'>try again.</a></p>";
